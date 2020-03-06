465b817c6d50f09a6be088394abc9af63f7737b4
Elliott Morris <elliottmorris848@gmail.com>
2020-03-06 00:58:40 -0500

    more prior work

--- scripts/R/Refactored/prior_find_mu_b_trend.R
+++ scripts/R/Refactored/prior_find_mu_b_trend.R
@@ -1,59 +1,90 @@
-# script to search for mu_b using state-level trends
+# script to search for mu_b using a national forecast and state-level 
+# trends in partisan lean, all optimized with caret
+
 library(tidyverse)
 library(purrr)
 library(urbnmapr)
 library(caret)
+library(caretEnsemble)
 library(colorspace)
+library(pbapply)
+library(lqmm)
+library(mvtnorm)
 
 # fit national model ------------------------------------------------------
-abramowitz <- read.csv('data/abramowitz_data.csv') %>% filter(year != 2020)
+abramowitz <- read.csv('data/abramowitz_data.csv')  %>%
+  left_join(read.csv('data/ANES_swing_voters.csv')) %>%
+  filter(year>1948)
 
-# train a caret model to predict demvote with incvote ~ q2gdp + juneapp + year:q2gdp + year:juneapp 
-prior_model <- caret::train(
-  incvote ~ q2gdp + juneapp + year:q2gdp + year:juneapp,
+# train a caret model to predict demvote with incvote ~ q2gdp + juneapp + swing voters
+# use a bunch of different regularization methods and combine them
+model_list  <- c(
+  "glmnet","brnn","blassoAveraged",
+  "lars2","penalized","rqlasso","relaxo",
+  "blasso","bridge"
+  )
+
+prior_model <- caretList(
+  incvote ~ q2gdp*juneapp + q2gdp:ANES_share_swing_voters + juneapp:ANES_share_swing_voters,
   data = abramowitz,
-  method = "glmnet",
+  #method = "glmboost",
+  methodList = model_list, 
+  metric = "RMSE",
   trControl = trainControl(
-    method = "LOOCV",
-    verboseIter = T),
+    #method = "LOOCV",
+    method = "repeatedcv", number = 7, repeats = 10, savePredictions = 'final',
+    allowParallel = T,verboseIter = T),
   tuneLength = 10)
 
-# find the optimal parameters
-best = which(rownames(prior_model$results) == rownames(prior_model$bestTune))
-best_result = prior_model$results[best, ]
-rownames(best_result) = NULL
-best_result
+# combine into one regularized ensemble learner
+stack <- caretStack(prior_model, 
+                    method = "glmboost",
+                    metric = "RMSE",
+                    trControl = trainControl(
+                      method = "repeatedcv", number = 7, repeats = 10, savePredictions = 'final',
+                      allowParallel = T,verboseIter = T),
+                    tuneLength = 10)
 
-coef(prior_model$finalModel,best_result$lambda)
+stack
+
+# find the optimal parameters
+# best = which(rownames(prior_model$results) == rownames(prior_model$bestTune))
+# best_result = prior_model$results[best, ]
+# rownames(best_result) = NULL
+# best_result
+# 
+# coef(prior_model$finalModel)#,best_result$lambda)
 
 # make predictions
-NATIONAL_PRIOR_DEM_TWO_PARTY_SHARE <- predict(prior_model,newdata = tibble(q2gdp = 1,
-                                                          juneapp = -10,
-                                                          year = 2020)) / 100
+NATIONAL_PRIOR_INCUMBENT_TWO_PARTY_SHARE <- predict(stack,
+                                                    newdata = tibble(q2gdp = 1,
+                                                                     juneapp = -10,
+                                                                     ANES_share_swing_voters = 0.05)) / 100
 
-NATIONAL_MODEL_ERROR <- best_result$RMSE / 100
+# negate when a rep is president
+NATIONAL_PRIOR_DEM_TWO_PARTY_SHARE <- 1 - NATIONAL_PRIOR_INCUMBENT_TWO_PARTY_SHARE
 
-cat(sprintf('Prior Clinton two-party vote is %s\nWith a standard error of %s',
-            round(NATIONAL_PRIOR_DEM_TWO_PARTY_SHARE,1),
-            round(NATIONAL_MODEL_ERROR,1)))
+NATIONAL_MODEL_ERROR <- sqrt(mean(stack$error$RMSE^2,na.rm=T)) / 100
 
-
+cat(sprintf('Prior Democratic two-party vote is %s\nWith a standard error of %s',
+            round(NATIONAL_PRIOR_DEM_TWO_PARTY_SHARE,3),
+            round(NATIONAL_MODEL_ERROR,3)))
 
 # grid of predictions
 expand.grid(q2gdp = c(-2,-1,0,1,2),
             juneapp = seq(-16,0),
-            year=2020) %>%
-  mutate(incumbent_share = predict(prior_model,.)) %>%
+            year=2020,
+            ANES_share_swing_voters = 0.1) %>%
+  mutate(incumbent_share = predict(stack,.)) %>%
   ggplot(.,aes(x=juneapp,y=incumbent_share,col=factor(q2gdp),fill=factor(q2gdp),group=factor(q2gdp))) +
   geom_hline(yintercept = 50,linetype=2,col='gray70') +
   geom_vline(xintercept = 0,linetype=2,col='gray70') +
   # error
-  geom_ribbon(aes(ymin = incumbent_share - best_result$RMSE*2,
-                  ymax = incumbent_share + best_result$RMSE*2),
-              col=NA,alpha=0.1,show.legend = F) +
+  # geom_ribbon(aes(ymin = incumbent_share - best_result$RMSE*2,
+  #                 ymax = incumbent_share + best_result$RMSE*2),
+  #             col=NA,alpha=0.1,show.legend = F) +
   # prediction
   geom_line() +
-  
   # trump share needed for two-party win
   geom_hline(yintercept = 48.6,linetype=2,col='black') +
   annotate(geom='text',x=-16,y=48.7,label='Estimated Trump share needed for electoral college victory',
@@ -64,7 +95,7 @@ expand.grid(q2gdp = c(-2,-1,0,1,2),
        title='Predicted Trump vote share of the national popular vote, two-party %') +
   scale_color_discrete_qualitative(name='Q2 GDP growth',palette = 'Warm') +
   scale_x_continuous(breaks=seq(-20,20,2)) +
-  scale_y_continuous(breaks=seq(0,100,2)) +
+  scale_y_continuous(breaks=seq(0,100,1)) +
   theme_minimal() +
   theme(legend.position = 'top',
         legend.justification = 'left',
@@ -95,25 +126,25 @@ ggplot(results %>%
   theme_minimal()
 
 
-
 # fit models in each state ------------------------------------------------
 # optimize the loess span for each state
-models <- results %>%
-  #filter(state %in% c('AZ','FL','WI')) %>%
-  group_by(state) %>%
-  do(mod = train(dem_two_party_share_lean ~ year,
-               data=.,
-               method='gamboost',
-               trControl = trainControl(method='LOOCV',allowParallel = TRUE, verboseIter=TRUE), 
-               # tuneGrid = expand.grid(degree=1,
-               #                        span=c(0.2,0.3,0.4,0.5,0.6,0.7)),
-               tuneLength = 10,
-               )
-  )
-
+# models <- results %>%
+#   #filter(state %in% c('AZ','FL','WI')) %>%
+#   group_by(state) %>%
+#   do(mod = train(dem_two_party_share_lean ~ year,
+#                data=.,
+#                method='gamboost',
+#                trControl = trainControl(method='LOOCV',allowParallel = TRUE, verboseIter=TRUE), 
+#                # tuneGrid = expand.grid(degree=1,
+#                #                        span=c(0.2,0.3,0.4,0.5,0.6,0.7)),
+#                tuneLength = 10,
+#                )
+#   )
+# 
+# write_rds(models,'data/state_prior_models.rds',compress = 'gz')
+models <- read_rds('data/state_prior_models.rds')
 
 beepr::beep(2)
-
 
 # get model error by state
 model_error <- tibble(state = models$state,
@@ -192,10 +223,20 @@ predictions %>%
   scale_color_viridis_d(name='State')
 
 
-# add given vote share for 2020
+
+# make predictions for 2020 -----------------------------------------------
+# this can make predictions with the model
 predictions_2020 <- predictions %>%
   filter(year==2020) %>%
   mutate(dem_two_party_share_hat = dem_two_party_share_lean_hat + NATIONAL_PRIOR_DEM_TWO_PARTY_SHARE)
+
+# but actually, just using the lagged value is better
+predictions_2020 <- predictions %>%
+  group_by(state) %>%
+  filter(year %in% c(2016,2020)) %>%
+  mutate(dem_two_party_share_hat = lag(dem_two_party_share_lean) + NATIONAL_PRIOR_DEM_TWO_PARTY_SHARE) %>%
+  filter(year==2020)  %>%
+  ungroup()
 
 # plot without leaners
 urbnmapr::states %>%
@@ -264,11 +305,7 @@ urbnmapr::states %>%
         plot.caption=element_text(hjust=0))
 
 
-
 # simulations? ------------------------------------------------------------
-library(lqmm)
-library(mvtnorm)
-
 # get correlation matrix
 cor.mat <- predictions %>%
   filter(year >= 2000) %>%
@@ -292,13 +329,14 @@ errors <- rmvnorm(1000000,
                   mean = rep(0,nrow(predictions_2020)),
                   sigma = cor.mat) 
 
-plot(predictions_2020$dem_two_party_share_hat, errors[runif(1,0,nrow(errors)),])
+#plot(predictions_2020$dem_two_party_share_hat, errors[runif(1,0,nrow(errors)),])
 
 # add thsoe errors times state-level sigma and error to the predicted mean to get trials
-trials <- lapply(1:ncol(errors),
+trials <- pblapply(1:ncol(errors),
+                   cl=parallel::detectCores()-1,
       function(x){
         # national error is 0.026
-        national_model_error <- NATIONAL_MODEL_ERROR/100 # needs to be in two-party terms
+        national_model_error <- NATIONAL_MODEL_ERROR # needs to be in two-party terms
         # state model error plus extrapolation error
         state_model_error <- model_error$error[x] + 0.02
         
@@ -342,7 +380,6 @@ p_dem_win
 predictions_2020 %>%
   left_join(read_csv("data/state_evs.csv")) %>%
   summarise(dem_ev = sum(ev * (dem_two_party_share_hat >= 0.5))) 
-
 
 # plot
 ggplot(sim_evs,aes(x=dem_ev,fill=ifelse(dem_ev>=270,'Democratic','Republican'))) +
